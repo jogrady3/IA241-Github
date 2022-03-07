ccc9058867b65b543b099548b910036bdb14523c
EC2 Default User <ec2-user@ip-172-31-34-121.ec2.internal>
2022-03-07 19:58:48 +0000

    Lab 6 and Lec 6

---
+++ lab6.py
@@ -0,0 +1,62 @@
+'''
+lab 6
+'''
+
+#3.1
+
+for i in range(6):
+    
+    if i != 3:
+    
+        print(i)
+        
+#3.2
+
+result = 1
+
+for i in range (1, 6):
+    
+    result = result * 1
+    
+print(result)
+
+#3.3
+
+for i in range(3, 6):
+    
+    result = result + 1
+    
+print(result)
+
+#3.4
+
+result = 1
+
+for i in range(3, 9):
+    
+    result = result * i
+    
+print(result)
+
+#3.5
+
+result = 1
+
+for i in range(4, 9):
+
+    result = result * i
+    
+print(result)
+
+#3.6
+
+result = 0
+
+for word in 'this is my 6th string'.split():
+
+    print(word)
+    #result = result + 1
+    
+#print(result)
+
+
